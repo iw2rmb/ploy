32fb74c5f4e5d96371ada3774f28ad4ba6c2d156
iw2rmb <iw2rmb@icloud.com>
2025-09-04 12:42:28 +0300

    Phase 6.1: decouple internal/debug from api/* via internal policy+builders facades; add unit test; support PLOY_SKIP_DEPLOY for testability

---
+++ internal/policy/policy.go
@@ -0,0 +1,26 @@
+package policy
+
+// ArtifactInput captures attributes used for policy enforcement
+type ArtifactInput struct {
+    Signed      bool
+    SBOMPresent bool
+    Env         string
+    SSHEnabled  bool
+    BreakGlass  bool
+    App         string
+    Lane        string
+    Debug       bool
+}
+
+// Enforcer defines policy enforcement behavior
+type Enforcer interface {
+    Enforce(ArtifactInput) error
+}
+
+// DefaultEnforcer is used by callers; can be overridden in tests
+var DefaultEnforcer Enforcer = allowAll{}
+
+type allowAll struct{}
+
+func (allowAll) Enforce(ArtifactInput) error { return nil }
+
