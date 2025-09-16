b6134acd1792d2340ae2a61ef0887ce57d7d9bd9
iw2rmb <iw2rmb@icloud.com>
2025-09-16 12:32:02 +0300

    tests(orchestration): cover GetJobEndpoint passthrough using fake adapter

---
+++ internal/orchestration/monitor_endpoint_test.go
@@ -0,0 +1,16 @@
+package orchestration
+
+import "testing"
+
+func TestHealthMonitor_GetJobEndpoint_Passthrough(t *testing.T) {
+    adapter := &fakeNomadAdapter{allocs: []AllocationStatus{{ID: "alloc-1", ClientStatus: "running"}}, endpoint: "http://127.0.0.1:12345"}
+    hm := NewHealthMonitorWithClient(adapter)
+    ep, err := hm.GetJobEndpoint("jobA")
+    if err != nil {
+        t.Fatalf("unexpected error: %v", err)
+    }
+    if ep != "http://127.0.0.1:12345" {
+        t.Fatalf("expected endpoint passthrough, got %s", ep)
+    }
+}
+
